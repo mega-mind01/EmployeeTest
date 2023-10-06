c62778da8cfcf88f0be72f2cb281d0e362a97ec9
Timothy Meleki <muyiwameleki@gmail.com>
2023-10-06 19:43:57 +0100

    initial commit

---
+++ EmployeeTest/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    internal class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private decimal monthlySalary;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    monthlySalary = value;
+                }
+            }
+        }
+
+        public Employee(string name1, string name2, decimal salary)
+        {
+            FirstName = name1;
+            LastName = name2;
+            MonthlySalary = salary;
+        }
+
+        public decimal annualSalary ()
+        {
+            decimal annual = monthlySalary * 12;
+            return annual;
+        }
+
+
+
+        public decimal salaryRaise (decimal newSalary)
+        {
+            newSalary = (newSalary / 10) + newSalary;
+            newSalary = newSalary * 12;
+            return newSalary;
+        }
+    }
+}
---
+++ EmployeeTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Onyinye", "Joann", 10.00M);
+            Employee employee2 = new Employee("Presido", "Ikhu", 1.00M);
+
+            string firstEmployee = employee1.FirstName + " " + employee1.LastName;
+            string secondEmployee = employee2.FirstName + " " + employee2.LastName;
+
+            Console.WriteLine($"Annual Salary of {firstEmployee} is: {employee1.annualSalary()}\n");
+            Console.WriteLine($"Annual Salary of {secondEmployee} is: {employee2.annualSalary()}\n");
+
+            Console.WriteLine("Giving a raise of 10% to their monthly salary\nNew salary is: \n");
+
+            decimal employee1Salary = employee1.MonthlySalary;
+            decimal employee2Salary = employee2.MonthlySalary;
+
+            employee1Salary = employee1.salaryRaise(employee1Salary);
+            employee2Salary = employee2.salaryRaise(employee2Salary);
+
+            Console.WriteLine($"Raised Salary of {firstEmployee} is: {employee1Salary}\n");
+            Console.WriteLine($"Raised Salary of {secondEmployee} is: {employee2Salary}\n");
+
+        }
+    }
+}
